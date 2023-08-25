a9bad5393ac08aa4a79e07f00230bb7956864a5e
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2023-08-25 14:11:02 -0400

    Added cmake files.

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,20 @@
 // by Sebastian Ibanez
 
 #include <iostream>
-#include <sqlite3.h>
+#include <../sqlite/sqlite3.h>
 #include <string>
 
 int main() {
-
+  // Test Print
   std::string advice = "Sage advice";
   std::string printCommand = "figlet -f slant ";
-
   system((printCommand + advice).c_str());
 
   const char *dbFile = "../sqlite/storage.db";
-  // sqlite3 db;
-  // sqlite3_open(dbFile, &db);
+  sqlite3 *db;
+  sqlite3_open(dbFile, &db);
+
+  //sqlite3
 
   return 0;
 }
